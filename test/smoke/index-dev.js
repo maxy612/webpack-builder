5bd95920e4f275a9f67ca8749921e9660354c668
马晓阳 <630259701@qq.com>
2020-05-14 16:02:06 +0800

    fix: 处理dev下报错"

---
+++ test/smoke/index-dev.js
@@ -0,0 +1,30 @@
+const path = require("path");
+const webpack = require("webpack");
+const rimraf = require("rimraf");
+const Mocha = require("mocha");
+
+const mocha = new Mocha({
+  timeout: "10000ms",
+});
+
+process.chdir(path.join(__dirname, "./template"));
+
+rimraf("./dist", () => {
+  const { WebpackDev } = require("../../index");
+
+  webpack(WebpackDev, (err, stats) => {
+    if (err) {
+      console.error(err);
+      process.exit(2);
+      return;
+    }
+
+    console.log(
+      stats.toString({
+        colors: true,
+        modules: false,
+        children: false,
+      })
+    );
+  });
+});
